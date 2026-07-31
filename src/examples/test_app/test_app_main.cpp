500c4cf621185b8dc029730a73b7b13fc11f5620
yzy <zengyangyan50@gmail.com>
2026-07-31 15:18:56 +0800

    Remove test_app

--- src/examples/test_app/test_app_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <px4_platform_common/log.h>
-
-extern "C" __EXPORT int test_app_main(int argc, char *argv[]);
-
-int test_app_main(int argc, char *argv[])
-{
-    PX4_INFO("Hello, PX4!");
-    return 0;
-}
